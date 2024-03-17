849c66faebc5e989ad57521d914533fe4ff88ff6
singer-workstation <singer.yang.yxg@gmail.com>
2024-03-17 18:50:43 +0300

    [update] license

--- deeplens/basics.py
+++ deeplens/basics.py
@@ -1,3 +1,12 @@
+"""
+Technical Paper:
+Yang, Xinge and Fu, Qiang and Heidrich, Wolfgang, "Curriculum learning for ab initio deep learned refractive optics," ArXiv preprint (2023)
+
+This code and data is released under the Creative Commons Attribution-NonCommercial 4.0 International license (CC BY-NC.) In a nutshell:
+    # The license is only for non-commercial use (commercial licenses can be obtained from authors).
+    # The material is provided as-is, with no warranties whatsoever.
+    # If you publish any code, data, or scientific work based on this, please cite our work.
+"""
 import math
 import torch
 import numpy as np
